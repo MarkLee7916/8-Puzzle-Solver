a4ff4d00a9795cb8c1751a2f084154f1ad1aac59
Mark Lee <57327675+MarkLee7916@users.noreply.github.com>
2020-01-11 20:52:46 +0000

    Update Main.java

--- Sliding8Puzzle/src/puzzle/Main.java
+++ Sliding8Puzzle/src/puzzle/Main.java
@@ -11,7 +11,8 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		Board start = generateBoard();
+		Board start = generateBoard(3);
+
 		System.out.println("Puzzle to solve is " + start);
 
 		Solver solve = new Solver(start);
@@ -20,54 +21,36 @@ public class Main {
 		System.out.println("This board was solved in " + (System.currentTimeMillis() - startTime) + " ms");
 	}
 
-	//Takes a solved board and use a rng to shuffle it, generating a new one
-	private static Board generateBoard() {
-		int[][] puzzle = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
-		Board start = new Board(puzzle);
+	// Takes a solved board and use a rng to shuffle it, generating a new one
+	private static Board generateBoard(int size) {
+		int[][] puzzle = new int[size][size];
+
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				puzzle[i][j] = i * size + j;
+
+		Board start = new Board(puzzle, size);
 		List<Board> a = null;
 
 		for (int i = 0; i < 100000; i++) {
 			a = start.neighbours();
-		    start = a.get(ThreadLocalRandom.current().nextInt(a.size()));
+			start = a.get(ThreadLocalRandom.current().nextInt(a.size()));
 		}
 		return start;
 	}
-	
-	
+
+	// Alternative method of generating the puzzle. Tends to generate harder boards to solve
 	@SuppressWarnings("unused")
-	private static int[][] generatePuzzle() {
-		int[][] puzzle = new int[3][3];
+	private static Board generatePuzzle(int size) {
+		int[][] puzzle = new int[size][size];
 		List<Integer> l = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 0));
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				puzzle[i][j] = l.get(ThreadLocalRandom.current().nextInt(l.size()));
 				l.remove(Integer.valueOf(puzzle[i][j]));
 			}
 		}
-		return puzzle;
-	}
-	
-	@SuppressWarnings("unused")
-	private static boolean isSolvable(int[][] a) {
-		int inversions = 0;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				inversions += getPrecedes(i, j, a);
-			}
-		}
-		return !(inversions % 2 == 0);
-	}
-
-	@SuppressWarnings("unused")
-	private static int getPrecedes(int x, int y, int[][] a) {
-		int precedes = 0;
-		for (int i = x; i < 3; i++) {
-			for (int j = y; j < 3; j++) {
-				if (a[i][j] < a[x][y])
-					precedes++;
-			}
-		}
-		return precedes;
+		return new Board(puzzle, size);
 	}
 }
